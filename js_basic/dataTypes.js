2bf588ac8fa458a7549e69aec50c827c7d02f297
R4jsingh884han <156075694+R4jsingh884han@users.noreply.github.com>
2024-08-14 10:37:02 +0000

    dataTypes

---
+++ js_basic/dataTypes.js
@@ -0,0 +1,34 @@
+"use strict" //treat all js code as newer version
+
+alert("Roshan Raj")
+// alert (3 + 3) //we are using nodejs, not browser
+
+//console.log(3 + 3) console.log("Roshan Raj")
+//console.log(3 + 3); console.log("Roshan Raj")
+
+console.log(3
+     +
+      3) // code readability should be high 
+      
+console.log("Roshan Raj")
+
+
+
+let name = "Roshan Raj"
+let age = 18
+let isLoggenIn = false
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// oject
+
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefine
